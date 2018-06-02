48ef46ebc7f637e1ce12f7f1c67ef1d32473c821
Fabio Hsu <Fabio90h@Fabios-MBP.attlocal.net>
2018-06-02 17:44:09 -0400

    Change Header styling

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='header'>
           <h1>TO DO LIST</h1>
           <TaskForm />
+        </div>
           <TaskList className='taskList'/>
       </div>
     );
